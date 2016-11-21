eafc88261bb2ae6bd673a0549d2b180684fceef5
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-21 15:56:48 -0800

    added a function that plays the note if you press its associating keyboard key

--- noise.js
+++ noise.js
@@ -5,6 +5,47 @@ $(document).ready( function() {
         var audioObject = '#' + note + 'Audio';
         console.log(audioObject);
         $(audioObject)[0].play();
-  });
+    });
+
+    $('body').keydown(function(event) {
+        var note = '';
+        if (event.key == 'a') {
+            note = 'a';
+        } else if (event.key == 'b') {
+            note = 'b';
+        } else if (event.key == 'c') {
+            note = 'c';
+        } else if (event.key == 'd') {
+            note = 'd';
+        } else if (event.key == 'e') {
+            note = 'e';
+        } else if (event.key == 'f') {
+            note = 'f';
+        } else if (event.key == 'g') {
+            note = 'g';
+        }
+        var audioObject = '#' + note + 'Audio';
+        console.log(audioObject);
+        $(audioObject)[0].play();
+    });
 
 });
+
+
+
+
+
+// var changeColor = function() {
+//     var box = $('#color-box');
+//     box.removeClass();
+//     box.addClass(nextColor());
+// };
+//
+// $(document).ready(function() {
+//     $('body').keydown(function(event) {
+//         // 32 is space; 13 is enter
+//         if (event.which == 32 || event.which == 13) {
+//             changeColor();
+//         }
+//     });
+// });
