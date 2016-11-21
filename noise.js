80886d9f9f6e52386f74ae3b096dea0beb5e1d76
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-21 15:25:19 -0800

    wrote some code so that a click anywhere makes the 'c' note.

--- noise.js
+++ noise.js
@@ -1,3 +1,12 @@
 $(document).ready( function() {
-  // your code here
+    $('body').click(function(event) {
+        $('#cAudio')[0].play();
+    });
+
+    $('.instrument').on('click', 'button', function(event) {
+        var note = $(this).html();
+        console.log(note);
+        $('#cAudio')[0].play();
+  });
+
 });
