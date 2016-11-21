4f2625346792e8da532b9c01a461f2d75414aa2b
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-21 15:33:47 -0800

    refactored code so that a click in the box plays the note from that box

--- noise.js
+++ noise.js
@@ -1,12 +1,10 @@
 $(document).ready( function() {
-    $('body').click(function(event) {
-        $('#cAudio')[0].play();
-    });
-
     $('.instrument').on('click', 'button', function(event) {
         var note = $(this).html();
         console.log(note);
-        $('#cAudio')[0].play();
+        var audioObject = '#' + note + 'Audio';
+        console.log(audioObject);
+        $(audioObject)[0].play();
   });
 
 });
